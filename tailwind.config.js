47dbcdffa954959afd949157306f9c7370341469
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2022-01-08 19:59:07 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+        './sections/**/*.{js,ts,jsx,tsx}',
+        './styles/**/*.{js,ts,jsx,tsx}',
+        './utils/**/*.{js,ts,jsx,tsx}',
+    ],
+    darkMode: 'class', // or 'media' or 'class' or false
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
