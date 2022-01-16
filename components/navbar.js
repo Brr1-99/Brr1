541dbc078ce00e316fd234b3b73ae5b93ad65a00
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2022-01-16 13:21:26 +0100

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,24 +1,25 @@
 
 const Navbar = () => {
 	return (
-		<div class='bg-black-100 sticky z-10'>
-			<div class='items-center flex relative justify-between'>
-				<div class='flex justify-start'>
-					<div>
-						<a> Home </a>
-					</div>
-					<div>
-						<a> About </a>
-					</div>
-					<div>
-						<a> Contact </a>
-					</div>
-				</div>
-				<div>
-
+		<nav class='bg-gray-400 sticky z-10 backdrop-blur-xl'>
+			<div class='mx-auto px-2 sm:px-5 lg:px-7'>
+				<div class='items-center flex relative justify-between h-16'>
+						<div class='flex justify-start'>
+							<div>
+								<a class='text-white'> Home </a>
+								<a> About </a>
+								<a> Contact </a>
+							</div>
+						</div>
+						<div class='flex items-center'>
+							<div>
+								<a> Github </a>
+								<a> Twitter </a>
+							</div>
+						</div>
 				</div>
 			</div>
-		</div>
+		</nav>
 	)
 }
 
