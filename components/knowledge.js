54cacfb97a015bb51abd6a25acd2df98ecfe19ce
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2022-01-19 19:25:21 +0100

    All 6 mastered skills + hover shadow

--- components/knowledge.js
+++ components/knowledge.js
@@ -1,6 +1,10 @@
 import Grow from '../images/grow.svg'
 import Python from '../images/icons/python.svg'
 import Html from '../images/icons/html-5.svg'
+import Css from  '../images/icons/css.svg'
+import Flask from '../images/icons/flask.svg'
+import Pycharm from '../images/icons/pycharm.svg'
+import Mysql from '../images/icons/mysql.svg'
 
 const skills = () => {
 	return (
@@ -10,22 +14,48 @@ const skills = () => {
 			<h2 class='text-xl text-center'> Knowledge is power 🧠, and I try to get stronger 🦸🏻 by trying new things everyday.</h2>
 			<br/>
 			<div class='grid sm:grid-cols-1 xl:grid-cols-2 gap-4'>
-				<div class='rounded bg-zinc-700 shadow-zinc-500'>
-					<h3 class='text-xl p-3 text-center'>
-						Settled / Mastered
-					</h3>
-					<hr/>
-					<div class='grid sm:grid-cols-4 xl:grid-cols-6 gap-4'>
-						<div class='p-4 flex flex-col items-center justify-center'>
-							<img {...Python} class='opacity-90'></img>
-							<div>
-								<span>Python</span>
+				<div class='rounded bg-zinc-700 hover:shadow-cyan-500/50 shadow-md '>
+					<div>
+						<h3 class='text-xl p-3 text-center'>
+							Settled / Mastered
+						</h3>
+						<hr/>
+						<div class='grid sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-4'>
+							<div class='p-4 flex flex-col items-center justify-center'>
+								<img {...Python} class='opacity-90'></img>
+								<div>
+									<span>Python</span>
+								</div>
 							</div>
-						</div>
-						<div class='p-4 flex flex-col items-center justify-center'>
-							<img {...Html} class='opacity-90'></img>
-							<div>
-								<span>Html</span>
+							<div class='p-4 flex flex-col items-center justify-center'>
+								<img {...Html} class='opacity-90'></img>
+								<div>
+									<span>Html</span>
+								</div>
+							</div>
+							<div class='p-4 flex flex-col items-center justify-center'>
+								<img {...Css} class='opacity-90'></img>
+								<div>
+									<span>CSS</span>
+								</div>
+							</div>
+							<div class='p-4 flex flex-col items-center justify-center'>
+								<img {...Flask} class='opacity-90'></img>
+								<div>
+									<span>Flask</span>
+								</div>
+							</div>
+							<div class='p-4 flex flex-col items-center justify-center'>
+								<img {...Mysql} class='opacity-90'></img>
+								<div>
+									<span>MySQl/MariaDB</span>
+								</div>
+							</div>
+							<div class='p-4 flex flex-col items-center justify-center'>
+								<img {...Pycharm} class='opacity-90'></img>
+								<div>
+									<span>PyCharm</span>
+								</div>
 							</div>
 						</div>
 					</div>
