883a489b0bd31f42ca8279283b5924044be9d5c3
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2022-01-20 12:35:08 +0100

    start customization navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,21 +1,48 @@
+import { Github } from "../images/icons"
+
 const Navbar = () => {
 	return (
 		<nav className='bg-zinc-800 sticky z-10 backdrop-blur-xl'>
-			<div className='mx-auto px-2 sm:px-5 lg:px-7'>
+			<div className='mx-auto px-2 sm:px-6 lg:px-8'>
 				<div className='items-center flex relative justify-between h-16'>
-						<div className='flex justify-start'>
-							<div>
-								<a className='text-white'> Home </a>
-								<a> About </a>
-								<a> Contact </a>
+					<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+						<button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+						<span className="sr-only">Open main menu</span>
+						<svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+							<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+						</svg>
+						<svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+							<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+						</svg>
+						</button>
+					</div>
+					<div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+						<div className="flex-shrink-0 flex items-center">
+							<img className="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow"/>
+							<img className="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow"/>
+						</div>
+						<div className="hidden sm:block sm:ml-6">
+							<div className="flex space-x-4">
+								<a href="#" className="bg-lime-600 text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-cyan-500" aria-current="page">Home</a>
+								<a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
+								<a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
 							</div>
 						</div>
-						<div className='flex items-center'>
-							<div>
-								<a> Github </a>
-								<a> Twitter </a>
-							</div>
+					</div>
+					<div className="absolute right-0 flex">
+						<div className="flex items-center justify-center ml-10 hidden md:block hover:text-cyan-500">
+							<a href='https://github.com/Brr1-99'>
+								Icono Github
+							</a>
 						</div>
+					</div>
+				</div>
+				<div className="sm:hidden" id="mobile-menu">
+					<div className="px-2 pt-2 pb-3 space-y-1">
+					<a href="#" className="bg-lime-600 text-white block px-3 py-2 rounded-md text-base font-medium hover:bg-cyan-500" aria-current="page">Home</a>
+					<a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">About</a>
+					<a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
+					</div>
 				</div>
 			</div>
 		</nav>
