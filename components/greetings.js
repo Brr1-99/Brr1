7ffe2be48acdebd747322732b6c88e8e10df7376
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2022-01-20 18:59:26 +0100

    eslint images greetings

--- components/greetings.js
+++ components/greetings.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import ImageLeft from '../images/viewleft.svg'
 import ImageRight from '../images/viewright.svg'
+import Image from 'next/image'
 
 
 const Greet = () => {
 	return (
 		<div className='min-h-screen flex flex-col justify-center items-center bg-zinc-800'>
 			<div className='grid md:grid-cols-3 gap-3'>
-				<div className='flex justify-start items-center px-4 md:block'>
-					<img {...ImageLeft} className='opacity-90'></img>
+				<div className='flex justify-start items-center px-4 md:block opacity-90'>
+					<Image src={ImageLeft} alt=''/>
 				</div>
 				<div className='flex justify-center items-center'>
 					<h1 className='text-white'> Hola buenas tardes</h1>
 				</div>
-				<div className='flex justify-end items-center px-12 md:block'>
-					<img {...ImageRight} className='opacity-90'></img>
+				<div className='flex justify-end items-center px-12 md:block opacity-90'>
+					<Image src={ImageRight} alt=''/>
 				</div>
 			</div>
 		</div>
