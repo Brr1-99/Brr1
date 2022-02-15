4132c90db684d203e5d5dbf59d7fd0955affa71e
Brr1-99 <93494400+Brr1-99@users.noreply.github.com>
2022-02-15 12:38:26 +0100

    both greetings animations

--- components/greetings.js
+++ components/greetings.js
@@ -11,9 +11,9 @@ const Greet = () => {
                     <Image src={ImageLeft} alt='' />
                 </div>
                 <div className='text-center text-white text-3xl md:text-4xl'>
-                    <p className='up'> Welcome all,</p>
-					<p> to my personal website.</p>
-					<p> Hope you enjoy! </p> 
+                    <p className='up text-left'> Welcome all,</p>
+					<p className='middle'> to my personal website.</p>
+					<p className='down text-right'> Hope you enjoy! </p> 
 					<i className='bi bi-hand-thumbs-up'></i>
                 </div>
                 <div className='flex justify-end items-center px-12 md:block opacity-90'>
